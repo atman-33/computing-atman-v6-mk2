cec0a351068801f4710b6da653a3caf7aecdd478
atman-33 <gpbjk0304@gmail.com>
2024-10-06 23:26:06 +0900

    feat: セッションストレージ、ログイン処理を追加

---
+++ app/routes/auth/services/auth.server.ts
@@ -0,0 +1,45 @@
+import { User } from '@prisma/client';
+import bcrypt from 'bcryptjs';
+import { Authenticator, AuthorizationError } from 'remix-auth';
+import { FormStrategy } from 'remix-auth-form';
+import { env } from '~/config/env';
+import { prisma } from '~/lib/prisma';
+import { sessionStorage } from './session.server';
+
+const SESSION_SECRET = env.SESSION_SECRET;
+
+if (!SESSION_SECRET) {
+  throw new Error('SESSION_SECRET is not defined');
+}
+
+const authenticator = new Authenticator<Omit<User, 'password'>>(sessionStorage);
+
+const formStrategy = new FormStrategy(async ({ form }) => {
+  const email = form.get('email');
+  const password = form.get('password');
+
+  if (!(email && password)) {
+    throw new Error('Invalid Request');
+  }
+
+  const user = await prisma.user.findUnique({ where: { email: String(email) } });
+
+  if (!user) {
+    throw new AuthorizationError();
+  }
+
+  const passwordsMatch = await bcrypt.compare(String(password), user.password);
+
+  if (!passwordsMatch) {
+    throw new AuthorizationError();
+  }
+
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const { password: _, ...userWithoutPassword } = user;
+
+  return userWithoutPassword;
+});
+
+authenticator.use(formStrategy, 'user-pass');
+
+export { authenticator };
