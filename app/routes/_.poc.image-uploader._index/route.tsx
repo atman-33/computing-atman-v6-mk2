d04afb1f1ec3aa8515e39ebe2d8980ff623e6e61
atman-33 <gpbjk0304@gmail.com>
2024-11-09 22:45:41 +0900

    feat: ドラッグアンドドロップでファイルをアップロードする機能を追加

--- app/routes/_.poc.image-uploader._index/route.tsx
+++ app/routes/_.poc.image-uploader._index/route.tsx
@@ -1,7 +1,8 @@
 import '@uploadcare/react-uploader/core.css';
 import { useState } from 'react';
 import { Input } from '~/components/shadcn/ui/input';
-import { UploadcareFileUploaderRegular } from './uploadcare-file-uploader-regular';
+import { UploadcareFileUploaderRegular } from './components/uploadcare-file-uploader-regular';
+import UploadcareImageUploader from './components/uploadcare-image-uploader';
 
 const ImageUploaderPage = () => {
   const [imageUrl, setImageUrl] = useState('');
@@ -14,16 +15,20 @@ const ImageUploaderPage = () => {
     <div className="flex flex-col items-center">
       <div className="m-8 flex flex-col items-center gap-4">
         <h2 className="text-xl font-bold">Uploadcare</h2>
-        <div className="flex flex-col items-center gap-2">
+        <div className="flex flex-col items-center gap-2 rounded-md border-2 p-8">
           <h3>FileUploaderRegular</h3>
           <UploadcareFileUploaderRegular />
+        </div>
+        <div className="flex flex-col items-center gap-2 rounded-md border-2 p-8">
+          <h3>ImageUploader</h3>
+          <UploadcareImageUploader />
         </div>
       </div>
       <div className="flex flex-col gap-4">
         <div className="flex items-center gap-4">
           <span className="whitespace-nowrap">画像URL</span>
           <Input
-            className="w-full"
+            className="w-[50dvw]"
             type="text"
             value={imageUrl}
             onChange={(e) => handleImageUrlChange(e.target.value)}
