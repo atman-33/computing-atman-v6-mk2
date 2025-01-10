e2fde7337fc7301b8f297b90cc64333c5b3ed410
atman-33 <gpbjk0304@gmail.com>
2025-01-10 09:56:53 +0900

    fix: 記事一覧の無限スクロールがローディング中のまま終了しない不具合を修正
    - ローディング中のまま指定時間経過した場合、再度データ取得する処理を追加
    
    Refs: #24

--- app/routes/_app.users.$userId.posts._index/components/post-list.tsx
+++ app/routes/_app.users.$userId.posts._index/components/post-list.tsx
@@ -21,37 +21,56 @@ const PostList = ({ posts, pageInfo }: PostListProps) => {
   const [hasNextPage, setHasNextPage] = useState<boolean>(pageInfo.hasNextPage);
   const [isLoading, setIsLoading] = useState(false);
   const [isIntersecting, ref] = useIntersection();
+  const [retryCount, setRetryCount] = useState(0);
+  const maxRetries = 3;
+  const retryTimeout = 5000; // 5秒
 
   useEffect(() => {
     if (isIntersecting && hasNextPage && !isLoading) {
+      // console.log('Fetching more posts...');
       setIsLoading(true);
       fetcher.load(`/users/${userId}/posts?first=${POST_LIMIT}&after=${endCursor}`);
+      // console.log('fetcher state 1: ', fetcher.state);
     }
+
+    // NOTE:
+    // fetcher.loadが正常に終了せず、isLoadingがfalseのまま終わらない場合があるため、
+    // タイマーを設定して、指定秒数後にリトライする。
+    const timeoutId = setTimeout(() => {
+      // console.log('fetcher state 2: ', fetcher.state);
+      if (isLoading && retryCount <= maxRetries) {
+        // console.log('Retrying fetch due to timeout...');
+        setIsLoading(false); // 再試行のためにisLoadingをリセット
+        setRetryCount((prevCount) => prevCount + 1);
+      }
+    }, retryTimeout);
+    // console.log('timeoutId', timeoutId);
+
+    return () => {
+      // console.log(`${timeoutId} clean up...`);
+      clearTimeout(timeoutId);
+    };
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [isIntersecting, pageInfo, isLoading, userId]);
 
   useEffect(() => {
-    if (isIntersecting && hasNextPage && fetcher.data) {
+    // console.log('fetcher.data updated...');
+    if (fetcher.data) {
+      // console.log('New data fetched:', fetcher.data);
       const newData = fetcher.data as Awaited<ReturnType<typeof getPostsByUser>>;
-      // console.log('endCursor', newData.data?.pageInfo.endCursor);
-      // console.log('hasNextPage', newData.data?.pageInfo.hasNextPage);
       setEndCursor(newData.data?.pageInfo.endCursor ?? null);
       setHasNextPage(newData.data?.pageInfo.hasNextPage ?? false);
       setAllPosts((prevPosts) => {
-        if (prevPosts) {
-          return [...prevPosts, ...(newData.data?.edges ?? [])] as PostEdges;
-        }
+        return [...prevPosts!, ...(newData.data?.edges ?? [])] as PostEdges;
       });
       setIsLoading(false);
     }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [fetcher.data]);
 
   return (
     <>
       <div className="grid gap-4 py-8 md:grid-cols-1">
-        {allPosts &&
-          allPosts.map((post) => post && <PostListItem key={post.node?.id} post={post.node} />)}
+        {allPosts?.map((post) => post && <PostListItem key={post.node?.id} post={post.node} />)}
       </div>
       <div ref={ref} className="mt-4 flex justify-center">
         {isLoading && <Spinner />}
